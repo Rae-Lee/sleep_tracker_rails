ce71ef38cae063b109cc89a4fded0f6486a13d7c
rae_lee <nicole831220@gmail.com>
2025-08-05 00:29:43 +0800

    feat(track_management):add sleep record entity and repository

---
+++ app/domains/track_management/entities/sleep_record.rb
@@ -0,0 +1,45 @@
+module TrackManagement
+  module Entities
+    class SleepRecord < Boxenn::Entity
+      def self.primary_keys
+        %i[user_id sleep_at]
+      end
+
+      attribute? :user_id, Types::Integer.optional
+      attribute? :sleep_at, Types::Params::DateTime.optional
+      attribute? :wake_at, Types::Params::DateTime.optional
+      attribute? :duration, Types::Integer.optional
+      attribute? :sleep_timezone, Types::String.optional
+      attribute? :wake_timezone, Types::String.optional
+      attribute? :created_at, Types::Params::DateTime.optional
+
+      def sleep_timezone
+        return nil unless sleep_at.present?
+
+        sleep_timezone || 'UTC'
+      end
+
+      def wake_timezone
+        return nil unless wake_at.present?
+
+        wake_timezone || 'UTC'
+      end
+
+      def sleep_at_in_timezone
+        return nil unless sleep_at.present?
+
+        sleep_at.in_time_zone(sleep_timezone)
+      end
+
+      def wake_at_in_timezone
+        return nil unless wake_at.present?
+
+        wake_at.in_time_zone(wake_timezone)
+      end
+
+      def incomplete?
+        sleep_at && wake_at.nil?
+      end
+    end
+  end
+end
